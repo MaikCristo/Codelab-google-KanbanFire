c25821a2543dcec1cc16922d50eaca2f55d136e1
Maik Cristo <josimaik.cristo@hotmail.com>
2023-04-19 15:34:39 -0300

    angular/kanban-fire/

---
+++ angular/kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-8821e',
+    appId: '1:1042032979445:web:cd4504dcc30960de90c4bc',
+    databaseURL: 'https://kanbanfire-8821e-default-rtdb.firebaseio.com',
+    storageBucket: 'kanbanfire-8821e.appspot.com',
+    apiKey: 'AIzaSyA7vPnWHRlieeWfy7Ji-wrjQ5tmnY575G4',
+    authDomain: 'kanbanfire-8821e.firebaseapp.com',
+    messagingSenderId: '1042032979445',
+    measurementId: 'G-HQGG1KWVQJ',
+  },};
+
+  
---
+++ angular/kanban-fire/src/environments/environment.ts
@@ -0,0 +1,25 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'kanbanfire-8821e',
+    appId: '1:1042032979445:web:cd4504dcc30960de90c4bc',
+    databaseURL: 'https://kanbanfire-8821e-default-rtdb.firebaseio.com',
+    storageBucket: 'kanbanfire-8821e.appspot.com',
+    apiKey: 'AIzaSyA7vPnWHRlieeWfy7Ji-wrjQ5tmnY575G4',
+    authDomain: 'kanbanfire-8821e.firebaseapp.com',
+    messagingSenderId: '1042032979445',
+    measurementId: 'G-HQGG1KWVQJ',
+  },};
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7vPnWHRlieeWfy7Ji-wrjQ5tmnY575G4",
+  authDomain: "kanbanfire-8821e.firebaseapp.com",
+  projectId: "kanbanfire-8821e",
+  storageBucket: "kanbanfire-8821e.appspot.com",
+  messagingSenderId: "1042032979445",
+  appId: "1:1042032979445:web:cd4504dcc30960de90c4bc",
+  measurementId: "G-HQGG1KWVQJ"
+  
+};
+
